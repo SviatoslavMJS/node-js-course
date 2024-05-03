d4e69bc3ee2171a0bea1e89d3146516abb5ec1e6
Sviatoslav <toslav@ukr.net>
2024-05-03 17:39:56 +0300

    implement ejs templates

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const { ENDPOINT } = require("./constants");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   engine({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
